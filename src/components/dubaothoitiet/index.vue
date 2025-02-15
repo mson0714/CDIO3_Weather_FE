de1b7b93c7bf45ac8a3b7d41b4315580ee75205d
tranlanh3009 <tranlanh3009@gmail.com>
2025-02-15 09:18:36 +0700

    FE

---
+++ src/components/dubaothoitiet/index.vue
@@ -0,0 +1,44 @@
+<template >
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card border-0 border-3 border-top border-primary">
+                <div class="card-header text-center">
+                    <h4 class="text-primary">Dự Báo Thời Tiết</h4>
+                </div>
+                <div class="card-body">
+                    <i class="fa-solid fa-cloud-rain"></i>
+                    <label class="mt-2" for=""><h5>Lượng Mưa</h5></label>
+                    <input type="text" class="form-control mb-2" placeholder="Nhập lượng mưa (mm)">
+                    <i class="fa-solid fa-temperature-three-quarters"></i>
+                    <label class="mt-2" for=""><h5>Nhiệt Độ Cao Nhất</h5></label>
+                    <input type="text" class="form-control mb-2 " placeholder="Nhập nhiêt độ cao nhất (°C)">
+                    <i class="fa-solid fa-temperature-quarter"></i>
+                    <label class="mt-2" for=""><h5>Nhiệt Độ Thấp Nhất</h5></label>
+                    <input type="text" class="form-control mb-2 " placeholder="Nhập nhiệt độ thấp nhất (°C)">
+                    <label class="mt-2" for=""><h5>Gió</h5></label>
+                    <input type="text" class="form-control mb-2" placeholder="Nhập chỉ số gió (km/h)">
+                </div>
+                <div class="card-footer text-center">
+                    <button class="btn btn-primary ">Kết Quả</button>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="row">
+        <div class="col-lg-12">
+            <div class="card">
+            <div class="card-body">
+                
+            </div>
+        </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    
+}
+</script>
+<style >
+    
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,19 @@
+import { createRouter, createWebHistory } from "vue-router"; // cài vue-router: npm install vue-router@next --save
+
+const routes = [
+    {
+        path : '/',
+        component: ()=>import('../layout/wrapper/index.vue')
+    },
+    {
+        path : '/du-bao-thoi-tiet',
+        component: ()=>import('../components/dubaothoitiet/index.vue')
+    },
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: routes
+})
+
+export default router
